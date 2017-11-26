e4aa26b22bbe7714de94bac9bf752d69a67f3255
Sagar Panda <er.sagarpanda@gmail.com>
2017-11-26 10:23:46 +0000

    add eslint and update webpack

--- src/SongData.js
+++ src/SongData.js
@@ -1,80 +1,80 @@
 const SongData = [
-    {
-        title:'Safar - Jab Harry Met Sejal',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29902.mp3'
-    },
-    {
-        title:'Radha (Remix by DJ Shilpi Sharma) - ab Harry Met Sejal',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29897.mp3'
-    },
-    {
-        title:'Beech Beech Mein - ab Harry Met Sejal',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29888.mp3'
-    },
-    {
-        title:'Chandralekha - A Gentleman',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29980.mp3'
-    },
-    {
-        title:'Ullu Ka Pattha - Jagga Jasoos',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29802.mp3'
-    },
-    {
-        title:'Galti Se Mistake - Jagga Jasoos',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29824.mp3'
-    },
-    {
-        title:'Ik Vaari Aa - Raabta',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29801.mp3'
-    },
-    {
-        title:'Raabta Title Track - Raabta',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29652.mp3'
-    },
-    {
-        title:'Main Tera Boyfriend - Raabta',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29688.mp3'
-    },
-    {
-        title:'Jiyo Re Bahubali - Bahubali 2',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29588.mp3'
-    },
-    {
-        title:'Soja Zara - Bahubali 2',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29590.mp3'
-    },
-    {
-        title:'Tamma Tamma Again - Badrinath Ki Dulhania',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29383.mp3'
-    },
-    {
-        title:'Cheez Badi - Machine',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29499.mp3'
-    },
-    {
-        title:'Chatur Naar - Machine',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29451.mp3'
-    },
-    {
-        title:'Humma Humma - Ok Jaanu',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29150.mp3'
-    },
-    {
-        title:'Zaalima - Raees',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29297.mp3'
-    },
-    {
-        title:'Nashe Si Chadh Gayi - Befikre',
-        path:'https://smp3dl.com/fileDownload/Songs/128/28993.mp3'
-    },
-    {
-        title:'You and Me - Befikre',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29077.mp3'
-    },
-    {
-        title:'Je T aime - Befikre',
-        path:'https://smp3dl.com/fileDownload/Songs/128/29114.mp3'
-    }
+  {
+    title: 'Safar - Jab Harry Met Sejal',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29902.mp3'
+  },
+  {
+    title: 'Radha (Remix by DJ Shilpi Sharma) - ab Harry Met Sejal',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29897.mp3'
+  },
+  {
+    title: 'Beech Beech Mein - ab Harry Met Sejal',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29888.mp3'
+  },
+  {
+    title: 'Chandralekha - A Gentleman',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29980.mp3'
+  },
+  {
+    title: 'Ullu Ka Pattha - Jagga Jasoos',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29802.mp3'
+  },
+  {
+    title: 'Galti Se Mistake - Jagga Jasoos',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29824.mp3'
+  },
+  {
+    title: 'Ik Vaari Aa - Raabta',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29801.mp3'
+  },
+  {
+    title: 'Raabta Title Track - Raabta',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29652.mp3'
+  },
+  {
+    title: 'Main Tera Boyfriend - Raabta',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29688.mp3'
+  },
+  {
+    title: 'Jiyo Re Bahubali - Bahubali 2',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29588.mp3'
+  },
+  {
+    title: 'Soja Zara - Bahubali 2',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29590.mp3'
+  },
+  {
+    title: 'Tamma Tamma Again - Badrinath Ki Dulhania',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29383.mp3'
+  },
+  {
+    title: 'Cheez Badi - Machine',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29499.mp3'
+  },
+  {
+    title: 'Chatur Naar - Machine',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29451.mp3'
+  },
+  {
+    title: 'Humma Humma - Ok Jaanu',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29150.mp3'
+  },
+  {
+    title: 'Zaalima - Raees',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29297.mp3'
+  },
+  {
+    title: 'Nashe Si Chadh Gayi - Befikre',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/28993.mp3'
+  },
+  {
+    title: 'You and Me - Befikre',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29077.mp3'
+  },
+  {
+    title: 'Je T aime - Befikre',
+    path: 'https://smp3dl.com/fileDownload/Songs/128/29114.mp3'
+  }
 ];
 
 export default SongData;
